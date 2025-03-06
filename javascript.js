09413a76e7a0cb9ea6e06e7e85f6c033f9a8d4f5
swcodes <87975571+sw-codes@users.noreply.github.com>
2025-03-06 11:50:15 +0000

    project steps completed
    got the grid working properly
    implemented a button and prompt for user to set the grid size
    changed the grid colours

--- javascript.js
+++ javascript.js
@@ -1,20 +1,27 @@
 const container = document.querySelector("div");
+const btn = document.querySelector("button");
+let userInput = 0;
 
-for (let i = 0; i < 16; i++) {
-    const div = document.createElement("div");
-    div.style.height = "100px";
-    div.style.width = "100px";
-    div.style.border = "solid";
-    div.style.backgroundColor = "pink";
-    div.style.margin = "5px";
-    container.appendChild(div);
-    for (let j = 0; j < 16; j++) {
-        const div = document.createElement("div");
-        div.style.height = "100px";
-        div.style.width = "100px";
-        div.style.border = "solid";
-        div.style.backgroundColor = "pink";
-        div.style.margin = "5px";
+btn.addEventListener("click", () => {
+    userInput = prompt("Enter a height value");
+    makeGrid(userInput);
+});
+
+function makeGrid(userInput) {
+    while (container.lastElementChild) {
+        container.removeChild(container.lastElementChild);
+    }
+    let gridAmount = userInput * userInput;
+    let gridHeight = 500 / userInput + "px";
+    let gridWidth = 500 / userInput + "px";
+    for (let i = 0; i < gridAmount; i++) {
+        let div = document.createElement("div");
+        div.style.height = gridHeight;
+        div.style.width = gridWidth;
+        div.style.backgroundColor = "lightgrey";
+        div.addEventListener("mouseover", () => {
+            div.style.backgroundColor = "cyan";
+        })
         container.appendChild(div);
     }
 }
